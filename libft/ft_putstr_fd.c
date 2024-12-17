d46ce9832b5790f92164fcfb3f32fde9a3d06dcd
Alisa Valiullina <avaliull@f1r6s15.codam.nl>
2024-12-17 12:40:17 +0100

    replaced libft files with the updated version

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -1,21 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putstr_fd.c                                      :+:    :+:           */
 /*                                                    +:+ +:+         +:+     */
 /*   By: avaliull <avaliull@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 12:08:16 by avaliull          #+#    #+#             */
-/*   Updated: 2024/10/19 16:05:56 by avaliull         ###   ########.fr       */
+/*   Updated: 2024/12/15 13:50:02 by avaliull       ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <unistd.h>
 
+static inline void	ignore_result(ssize_t unused_result)
+{
+	(void) unused_result;
+}
+
 void	ft_putstr_fd(char *s, int fd)
 {
 	if (!s)
 		return ;
-	write(fd, s, ft_strlen(s));
+	ignore_result(write(fd, s, ft_strlen(s)));
 }
